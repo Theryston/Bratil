2592972c4107d029936f3915c396ffcba95aa6b2
Theryston <baitthenew@gmail.com>
2020-10-14 09:41:10 -0300

    versão 1.0

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
+app.use('/text_search_content', express.static('text_search_content'));
+
+
+app.get('/', function(req, res) {
+  res.render('ia');
+});
+
+app.get('/termos', function(req, res) {
+  res.render('termos');
+});
+
+
+
+app.get('*', function(req, res) {
+  res.render('no-rota')
+})
+
+app.listen(3000, function () {
+  console.log('rodando!')
+});

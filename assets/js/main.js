d1c92f7b4eb82a8128c4613e63ef9ad527ad5f2b
Theryston <funktodo2@gmail.com>
2020-10-27 17:28:46 -0300

    https

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,10 +33,10 @@ var autores = window.confirm('quer ativar as respostas automáticas? (baseadas n
 if (url.match('https')) {
   var name = window.prompt('qual é seu nome?')
   var autores = window.confirm('quer ativar as respostas automáticas? (baseadas nos textos em que você copiar)')
-} /*else {
+} else {
   url = url.replace(/http/gi, 'https')
   window.location = url;
-}*/
+}
 //autores = true
 
 window.addEventListener('load', () => {

7d8f307751bd6d98367657fe8a994b384c35eb89
Theryston <funktodo2@gmail.com>
2020-10-28 06:53:48 -0300

    bait search

---
+++ assets/js/search.js
@@ -0,0 +1,18 @@
+var ContainerSearch = document.querySelector('#container-search');
+var TextSearch = document.querySelector('#text-search')
+var IconSearch = document.querySelector('.fa-search')
+var section = document.querySelector('#section');
+var header = document.querySelector('#header')
+
+TextSearch.addEventListener('focus', () => {
+  header.style.display = 'none'
+  section.style.display = 'none'
+  ContainerSearch.style.top = '6em'
+  ContainerSearch.style.width = '95vw'
+});
+TextSearch.addEventListener('focusout', () => {
+   header.style.display = 'block'
+  section.style.display = 'block'
+  ContainerSearch.style.top = '50%'
+  ContainerSearch.style.width = '80vw'
+})

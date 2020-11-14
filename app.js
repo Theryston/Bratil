d72c575803ab9124f2c1da24e9f54f661555b476
Theryston Santos <funktodo2@gmail.com>
2020-11-14 11:52:15 -0300

    sitemap

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.get('/login', (req, res) => {
   res.render('login')
 })
 
+app.get('/sitemap', (req, res) => {
+  res.sendfile('./sitemap.xml')
+})
+
 app.get('*', function(req, res) {
   res.render('no-rota')
 })

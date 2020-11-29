066fe0eca199e7e7fb3c49b521e0438bba3ad736
Theryston Santos <funktodo2@gmail.com>
2020-11-29 08:52:35 -0300

    getJSON

--- routes/search.js
+++ routes/search.js
@@ -7,18 +7,18 @@ router.get('/', function(req, res) {
   res.render('search/index');
 });
 
-router.get('/response', (req, res) => {
+router.get('/response/page', (req, res) => {
+  res.render('search/response', {
+    question: req.query["question"]
+  })
+})
+
+router.get('/response/:question', (req, res) => {
   res.send({
     response: [
       ['Lorem ipsum imperdiet ad lobortis hac eleifend scelerisque ipsum fusce, vivamus tincidunt nisi fusce proin nostra habitasse rhoncus ullamcorper, donec sagittis non tempor convallis et quisque tempor. nulla fusce nostra a porta per integer, enim lacus egestas elementum vestibulum sapien porttitor, curae quisque ut quam cras.', 1],
       ['ante non diam sociosqu erat, dictumst sem elit lorem himenaeos donec rutrum adipiscing tempus sapien. netus dictum quam nulla aptent fermentum faucibus tempus, turpis sagittis sem nullam ad nisi dolor ad, elementum consequat blandit donec ut consectetur. ', 2]
       ]
-  })
-})
-
-router.get('/response/page', (req, res) => {
-  res.render('search/response', {
-    question: req.query["question"]
   })
 })
 
